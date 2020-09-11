a6a1c1bfb1f79e2bd3a0c42fbac41090544ff0d5
Aamir Hussain <aamir222686@yahoo.com>
2020-09-11 14:31:41 +0530

    Changed port to dynamic

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ app.put('/image', (req, res) => {
 })
 
 
-app.listen(3000, ()=> {
-	console.log('App is running on Port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`App is running on Port ${process.env.PORT}`);
 })
