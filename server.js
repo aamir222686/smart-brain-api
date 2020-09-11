adedd8e5d0faeb9e61ded9bc617f885adb222193
Aamir Hussain <aamir222686@yahoo.com>
2020-09-11 15:18:17 +0530

    db server url added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-deep-22497',
     user : 'postgres',
     password : 'admin',
     database : 'smart-brain'
